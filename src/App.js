fc36f6319be3c13423fea91816d808f50626e872
mike-mjk <michael.kostolnik@gmail.com>
2019-09-01 19:46:19 -0700

    add discrepancy ui and logic

--- src/App.js
+++ src/App.js
@@ -13,6 +13,25 @@ const flip = () => {
   }
 };
 
+const incrementHeads = state => {
+  return { heads: state.heads + 1 };
+};
+
+const incrementTails = state => {
+  return { tails: state.tails + 1 };
+};
+
+const setDiscrepancy = state => {
+  let currentDiscrepancy = Math.abs(state.heads - state.tails);
+  return { currentDiscrepancy: currentDiscrepancy };
+};
+
+const setBiggestDiscrepancySoFar = state => {
+  if (state.currentDiscrepancy > state.biggestDiscrepancy) {
+    return { biggestDiscrepancy: state.currentDiscrepancy };
+  }
+};
+
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -21,28 +40,34 @@ class App extends React.Component {
 
     this.state = {
       heads: 0,
-      tails: 0
+      tails: 0,
+      currentDiscrepancy: 0,
+      biggestDiscrepancy: 0
     };
   }
 
   handleFlip() {
     const result = flip();
     if (result === "h") {
-      this.setState(state => {
-        return { heads: state.heads + 1 };
-      });
+      this.setState(incrementHeads);
     } else {
-      this.setState(state => {
-        return { tails: state.tails + 1 };
-      });
+      this.setState(incrementTails);
     }
+
+    this.setState(setDiscrepancy);
+    this.setState(setBiggestDiscrepancySoFar);
   }
 
   render() {
+    const { heads, tails, currentDiscrepancy, biggestDiscrepancy } = this.state;
     return (
       <React.Fragment>
-        <Counter heads={this.state.heads} tails={this.state.tails} />
+        <Counter heads={heads} tails={tails} />
         <FlipOnceButton onFlip={this.handleFlip} />
+        <Discrepancy
+          current={currentDiscrepancy}
+          biggest={biggestDiscrepancy}
+        />
       </React.Fragment>
     );
   }
--- src/Discrepancy.js
+++ src/Discrepancy.js
@@ -4,8 +4,8 @@ const Discrepancy = props => {
   return (
     <div>
       <h3>Discrepancies</h3>
-      <div>Current: </div>
-      <div>Biggest so Far: </div>
+      <div>Current: {props.current} </div>
+      <div>Biggest so Far: {props.biggest} </div>
     </div>
   );
 };
